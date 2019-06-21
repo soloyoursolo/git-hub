bcc253ddcc78b924f62eca0ffed413f05d61a6b6
soloyoursolo <459533439@qq,com>
2019-06-21 09:21:05 +0800

    worker:adm-A加入project:phase-02

---
+++ phase-04-implementation-003-javaWeb/src/HelloWorldWeb.java
@@ -0,0 +1,14 @@
+
+public class HelloWorldWeb {
+    public  String poemTest="《\"流浪地球\"》<br /> <span style=\"font-size: 18px; color: grey\">\r\n" + 
+    		"			-方杨 </span><br /> \r\n" + 
+    		"			<br /> 没有人知道未来会是什么样子 \r\n" + 
+    		"			<br /> 我选择相信希望";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public String getPoemContent() {
+    	return poemTest;
+    }
+}
---
+++ testWeb/WebContent/NewFile.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	
+	<%
+	  String t="asdasldas";
+	%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+  String tt="10";
+%>
+</body>
+</html>
